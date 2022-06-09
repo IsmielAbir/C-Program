ebad48a65ff876ac5c7a6ae293b1a0b294482210
Ismielabir <ismielabir286@gmail.com>
2022-06-09 20:16:01 +0600

    word count

--- wordCount.c
+++ wordCount.c
@@ -4,12 +4,12 @@ int main(){
 
     char str[1000];
     int word=0;
-    scanf("%[^\n]%*s", str);
+    scanf("%[^\n]%*c", str);
     for(int i=0; str[i]!='\0';i++){
-        if(str[i] == ' ' && str[i+1]!= ' ')
+        if(str[i] == ' ' && str[i+1] != ' ')
         word++;
     }
-    printf("Number of words is %d\n", word+1);
+    printf("\nNumber of words is %d\n", word+1);
 
     return 0;
 }
